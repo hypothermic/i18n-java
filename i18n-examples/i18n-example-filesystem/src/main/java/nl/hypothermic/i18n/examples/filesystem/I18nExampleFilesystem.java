932445b553463413491df5f9f2b45513974b33fb
hypothermic <admin@hypothermic.nl>
2019-07-15 01:11:00 +0200

    Finish filesystem example, add json sample file
    Signed-off-by: hypothermic <admin@hypothermic.nl>

--- i18n-examples/i18n-example-filesystem/src/main/java/nl/hypothermic/i18n/examples/filesystem/I18nExampleFilesystem.java
+++ i18n-examples/i18n-example-filesystem/src/main/java/nl/hypothermic/i18n/examples/filesystem/I18nExampleFilesystem.java
@@ -3,7 +3,11 @@ package nl.hypothermic.i18n.examples.filesystem;
 import nl.hypothermic.i18n.I18nBuilder;
 import nl.hypothermic.i18n.SynchronousI18n;
 import nl.hypothermic.i18n.api.model.I18n;
+import nl.hypothermic.i18n.format.json.JsonI18nFormat;
 import nl.hypothermic.i18n.provider.fs.CachedFileSystemProvider;
+
+import java.io.File;
+import java.util.Locale;
 
 public class I18nExampleFilesystem {
 
@@ -11,14 +15,24 @@ public class I18nExampleFilesystem {
         new I18nExampleFilesystem().execute();
     }
 
+    public static final String ERROR_MESSAGE = "Unable to get the value...";
+
     private final I18n i18n = I18nBuilder.newBuilder()
                                             .setImplementation(SynchronousI18n.class)
-                                            .setProvider(new CachedFileSystemProvider())
+                                            .setFileFormat(new JsonI18nFormat())
+                                            .setProvider(new CachedFileSystemProvider(new File("i18n-examples")))
+                                            .setLocale(Locale.ENGLISH)
+                                            .initializeProviderAndCatchExceptions()
                                             .buildAndCatchExceptions();
 
     public void execute() {
-        String myInternationalizedString = i18n.getString("myapp.dialog.greeting", "Unable to get the value...");
+        // This should print "Welcome!" in console.
+        System.out.println(i18n.getString("myapp.dialog.greeting", ERROR_MESSAGE));
 
-        System.out.println(myInternationalizedString);
+        // Changing the locale at any time is not a problem
+        i18n.setLocale(Locale.GERMAN);
+
+        // This should print "Wilkommen!" in console.
+        System.out.println(i18n.getString("myapp.dialog.greeting", ERROR_MESSAGE));
     }
 }
